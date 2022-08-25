7411f2b29e83ba9a7be3627103ba0b6c01d00bef
Anh180693 <anhltth2203042@fpt.edu.vn>
2022-08-25 11:37:53 +0700

    demo

--- index.js
+++ index.js
@@ -6,10 +6,48 @@ app.listen(port,function (){
     console.log("Sever is running...");
 })
 
+const mysql = require("mysql");
+const  conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "t2204m",
+        port: 3306
+    }
+);
+
 app.get("/",function (req,res){
     res.send("Hello world!");
 })
 
 app.get("/bong-da",function (req,res){
     res.send("Bóng đá 24h");
+})
+
+// api danh sach category
+
+app.get("/api-get-category",function (reg,res){
+    const sql_txt = "select * from category";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+})
+
+app.get("/api-get-product",function (reg,res) {
+    const sql_1 = "select * from product";
+    conn.query(sql_1,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+})
+
+app.get("/api-get-product1",function (reg,res) {
+    const sql_2 = "select product.id,product.name,product.price,"+
+        "category.name as category_name from product left join "+
+        "category on product.categoryId = category.id;";
+    conn.query(sql_2,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
 })
